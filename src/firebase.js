cc0e6ae2b5f75a870ec1e74acab680cc696b8a42
AayushSheth2003 <aayushsheth2003@gmail.com>
2022-10-10 23:28:43 +0530

    phone verification added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoDfaRaWwOjlqFZ1s0Qjrirtz14Jx-dT8",
+  authDomain: "auto-buddys-otp.firebaseapp.com",
+  projectId: "auto-buddys-otp",
+  storageBucket: "auto-buddys-otp.appspot.com",
+  messagingSenderId: "161164505992",
+  appId: "1:161164505992:web:3264d40d0c70d60343803d",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
--- src/sections/authentication/register/RegisterForm.js
+++ src/sections/authentication/register/RegisterForm.js
@@ -1,14 +1,32 @@
-import * as Yup from 'yup';
-import { useState,useRef } from 'react';
-import { useFormik, Form, FormikProvider } from 'formik';
-import { useNavigate } from 'react-router-dom';
-// material
-import { Stack, TextField, IconButton, InputAdornment, Switch, Alert,FormGroup,FormControlLabel } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
-// component
-import Iconify from '../../../components/Iconify';
-import useAuth from "../../../hooks/useAuth"
-// ----------------------------------------------------------------------
+import * as Yup from "yup";
+import { useState, useRef } from "react";
+import { useFormik, Form, FormikProvider } from "formik";
+import { useNavigate } from "react-router-dom";
+import { RecaptchaVerifier, getAuth } from "firebase/auth";
+import { firebaseApp } from "src/firebase";
+
+import {
+  signInWithPhoneNumber,
+  initializeAuth,
+  browserSessionPersistence,
+  browserPopupRedirectResolver,
+} from "firebase/auth";
+
+import {
+  Stack,
+  TextField,
+  IconButton,
+  InputAdornment,
+  Switch,
+  Alert,
+  FormGroup,
+  FormControlLabel,
+  Button as MButton,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
+import Iconify from "../../../components/Iconify";
+import useAuth from "../../../hooks/useAuth";
 
 export default function RegisterForm() {
   const navigate = useNavigate();
@@ -20,85 +38,218 @@ export default function RegisterForm() {
   const pinRef = useRef();
   const emailRef = useRef();
   const phoneRef = useRef();
+  const otpRef = useRef();
   const passwordRef = useRef();
-  const [pat,setPat] = useState(false)
-  const [load,setLoad] = useState(false)
+  const [pat, setPat] = useState(false);
+  const [load, setLoad] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const { registerUser, err } = useAuth();
+  const [isPhoneVerified, setIsPhoneVerified] = useState(false);
+  const [isCaptchaVerified, SetIsCaptchaVerified] = useState(false);
 
   const RegisterSchema = Yup.object().shape({
     Name: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Name required'),
-    phone: Yup.string().min(10, 'Invalid!').max(11, 'Invalid!').required('Phone number required'),
-    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-    password: Yup.string().required('Password is required'),
-    // hospname: Yup.string().required('Hospital name is required'),
-    // address: Yup.string().required('Hospital address is required'),
-    // city: Yup.string().required('Hospital city is required'),
-    // state: Yup.string().required('Hospital state is required'),
-    // pin: Yup.number().required('Pincode required'),
-});
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Name required"),
+    phone: Yup.string()
+      .min(10, "Invalid!")
+      .max(11, "Invalid!")
+      .required("Phone number required"),
+    email: Yup.string()
+      .email("Email must be a valid email address")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
+    otp: Yup.string().required("Phone number verification required"),
+  });
 
   const formik = useFormik({
     initialValues: {
-      Name: '',
-      phone: '',
-      email: '',
-      password: '',
-      hospname: '',
-      state: '',
-      city: '',
-      pin: '',
-      address: '',
+      Name: "",
+      phone: "",
+      email: "",
+      password: "",
+      hospname: "",
+      state: "",
+      city: "",
+      pin: "",
+      address: "",
+      otp: "",
     },
     validationSchema: RegisterSchema,
     onSubmit: () => {
-      if(pat){
-        registerUser(nameRef.current.value,emailRef.current.value,phoneRef.current.value,passwordRef.current.value,pat,hospnameRef.current.value,addressRef.current.value,cityRef.current.value,stateRef.current.value,pinRef.current.value)
+      if (!isPhoneVerified) {
+        alert("Please Verify Your Phone Number");
+        return;
       }
-      else{
-        registerUser(nameRef.current.value,emailRef.current.value,phoneRef.current.value,passwordRef.current.value,pat)
+
+      if (pat) {
+        registerUser(
+          nameRef.current.value,
+          emailRef.current.value,
+          phoneRef.current.value,
+          passwordRef.current.value,
+          pat,
+          hospnameRef.current.value,
+          addressRef.current.value,
+          cityRef.current.value,
+          stateRef.current.value,
+          pinRef.current.value
+        );
+      } else {
+        registerUser(
+          nameRef.current.value,
+          emailRef.current.value,
+          phoneRef.current.value,
+          passwordRef.current.value,
+          pat
+        );
       }
-      
-      
-      if(err){setLoad(true)}
 
-    }
-
+      if (err) {
+        setLoad(true);
+      }
+    },
   });
 
-  const handleChange=()=>{
-    setPat(!pat)
-  }
+  const handleChange = () => {
+    setPat(!pat);
+  };
 
   const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
 
+  const recaptchaRef = useRef();
+
+  function onChange(value) {
+    console.log("Capcha value:", value);
+  }
+
+  const captchaRef = useRef(null);
+
+  const auth = initializeAuth(firebaseApp, {
+    persistence: browserSessionPersistence,
+    popupRedirectResolver: browserPopupRedirectResolver,
+  });
+
+  const [shouldShowCaptcha, setShouldShowCaptcha] = useState(false);
+  const [sendOtpDisabled, SetsendOtpDisabled] = useState(true);
+  const [verifyOtpDisabled, SetverifyOtpDisabled] = useState(true);
+
   return (
     <FormikProvider value={formik}>
-      {err?<Alert severity="error" sx={{mb:2}}>{err}</Alert>:null}
+      {err ? (
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {err}
+        </Alert>
+      ) : null}
       <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
         <Stack spacing={3}>
-          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-            <TextField
-              fullWidth
-              label="Name"
-              inputRef={nameRef}
-              {...getFieldProps('Name')}
-              error={Boolean(touched.Name && errors.Name)}
-              helperText={touched.Name && errors.Name}
-            />
-
+          <TextField
+            fullWidth
+            label="Name"
+            inputRef={nameRef}
+            {...getFieldProps("Name")}
+            error={Boolean(touched.Name && errors.Name)}
+            helperText={touched.Name && errors.Name}
+          />
+          <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
             <TextField
               fullWidth
               label="Phone"
               inputRef={phoneRef}
               type="text"
-              {...getFieldProps('phone')}
+              {...getFieldProps("phone")}
               error={Boolean(touched.phone && errors.phone)}
               helperText={touched.phone && errors.phone}
+              onKeyUp={(e) => {
+                SetsendOtpDisabled(phoneRef.current.value.length < 10);
+                setIsPhoneVerified(false);
+                SetIsCaptchaVerified(false);
+                SetverifyOtpDisabled(true);
+              }}
             />
+
+            <MButton
+              id="send_otp_btn"
+              style={{ width: "150px", alignSelf: "start", marginTop: "4px" }}
+              size="large"
+              variant="contained"
+              disabled={sendOtpDisabled}
+              onClick={() => {
+                if (phoneRef.current.value.length != 10) {
+                  return;
+                }
+
+                setShouldShowCaptcha(true);
+                console.log("here", shouldShowCaptcha);
+
+                const appVerifier = new RecaptchaVerifier(
+                  "my_recaptcha_container",
+                  {},
+                  auth
+                );
+                window.recaptchaVerifier = appVerifier;
+                console.log(appVerifier);
+
+                const phoneNumber = "+91" + phoneRef.current.value;
+                signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+                  .then((confirmationResult) => {
+                    window.confirmationResult = confirmationResult;
+                    SetIsCaptchaVerified(true);
+                  })
+                  .catch((error) => {
+                    SetIsCaptchaVerified(false);
+                    s;
+                  });
+              }}
+            >
+              Send OTP
+            </MButton>
+          </Stack>
+          <div id="my_recaptcha_container"></div>
+
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            spacing={2}
+            style={{
+              display: `${!shouldShowCaptcha ? "none" : "flex"}`,
+            }}
+          >
+            <TextField
+              fullWidth
+              label="OTP"
+              inputRef={otpRef}
+              type="text"
+              {...getFieldProps("otp")}
+              error={Boolean(touched.otp && errors.otp)}
+              helperText={touched.otp && errors.otp}
+              onKeyUp={(e) => {
+                SetverifyOtpDisabled(
+                  otpRef.current.value.length < 6 || !isCaptchaVerified
+                );
+              }}
+            />
+            <MButton
+              style={{ width: "150px", alignSelf: "start", marginTop: "4px" }}
+              size="large"
+              variant="contained"
+              onClick={() => {
+                const code = otpRef.current.value;
+                confirmationResult
+                  .confirm(code)
+                  .then((result) => {
+                    const user = result.user;
+
+                    setIsPhoneVerified(true);
+                  })
+                  .catch((error) => {
+                    setIsPhoneVerified(false);
+                  });
+              }}
+              disabled={verifyOtpDisabled}
+            >
+              {isPhoneVerified ? "✔️" : "Verify"}
+            </MButton>
           </Stack>
 
           <TextField
@@ -107,26 +258,38 @@ export default function RegisterForm() {
             autoComplete="username"
             type="email"
             label="Email address"
-            {...getFieldProps('email')}
+            {...getFieldProps("email")}
             error={Boolean(touched.email && errors.email)}
             helperText={touched.email && errors.email}
+            style={
+              !shouldShowCaptcha
+                ? {
+                    marginTop: "0",
+                  }
+                : {}
+            }
           />
 
           <TextField
             fullWidth
             inputRef={passwordRef}
             autoComplete="current-password"
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             label="Password"
-            {...getFieldProps('password')}
+            {...getFieldProps("password")}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
-                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                  <IconButton
+                    edge="end"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    <Iconify
+                      icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
+                    />
                   </IconButton>
                 </InputAdornment>
-              )
+              ),
             }}
             error={Boolean(touched.password && errors.password)}
             helperText={touched.password && errors.password}
@@ -134,77 +297,74 @@ export default function RegisterForm() {
           <FormGroup>
             <FormControlLabel
               onChange={handleChange}
-              control={<Switch defaultChecked/>}
+              control={<Switch defaultChecked />}
               label="I am the patient's relative."
             />
           </FormGroup>
 
-          {pat?
-          <>
-          <TextField
-            fullWidth
-            inputRef={hospnameRef}
-            autoComplete="hospital name"
-            type="text"
-            label="Hospital Name"
-            {...getFieldProps('hospname')}
-            error={Boolean(touched.hospname && errors.hospname)}
-            helperText={touched.hospname && errors.hospname}
-          />
+          {pat ? (
+            <>
+              <TextField
+                fullWidth
+                inputRef={hospnameRef}
+                autoComplete="hospital name"
+                type="text"
+                label="Hospital Name"
+                {...getFieldProps("hospname")}
+                error={Boolean(touched.hospname && errors.hospname)}
+                helperText={touched.hospname && errors.hospname}
+              />
 
-          <TextField
-            fullWidth
-            inputRef={addressRef}
-            autoComplete="hospital address"
-            type="text"
-            label="Hospital Address"
-            {...getFieldProps('hospaddr')}
-            error={Boolean(touched.hospaddr && errors.hospaddr)}
-            helperText={touched.hospaddr && errors.hospaddr}
-          />
+              <TextField
+                fullWidth
+                inputRef={addressRef}
+                autoComplete="hospital address"
+                type="text"
+                label="Hospital Address"
+                {...getFieldProps("hospaddr")}
+                error={Boolean(touched.hospaddr && errors.hospaddr)}
+                helperText={touched.hospaddr && errors.hospaddr}
+              />
 
-          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-            <TextField
-              fullWidth
-              label="City"
-              inputRef={cityRef}
-              {...getFieldProps('city')}
-              error={Boolean(touched.city && errors.city)}
-              helperText={touched.city && errors.city}
-            />
+              <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <TextField
+                  fullWidth
+                  label="City"
+                  inputRef={cityRef}
+                  {...getFieldProps("city")}
+                  error={Boolean(touched.city && errors.city)}
+                  helperText={touched.city && errors.city}
+                />
 
-            <TextField
-              fullWidth
-              label="Pincode"
-              inputRef={pinRef}
-              {...getFieldProps('pin')}
-              error={Boolean(touched.pin && errors.pin)}
-              helperText={touched.pin && errors.pin}
-            />
+                <TextField
+                  fullWidth
+                  label="Pincode"
+                  inputRef={pinRef}
+                  {...getFieldProps("pin")}
+                  error={Boolean(touched.pin && errors.pin)}
+                  helperText={touched.pin && errors.pin}
+                />
+              </Stack>
 
-            
-          </Stack>
-
-          <TextField
-              fullWidth
-              label="State"
-              inputRef={stateRef}
-              type="text"
-              {...getFieldProps('state')}
-              error={Boolean(touched.state && errors.state)}
-              helperText={touched.state && errors.state}
-            />
-          </>
-          
-          :null}
-          
+              <TextField
+                fullWidth
+                label="State"
+                inputRef={stateRef}
+                type="text"
+                {...getFieldProps("state")}
+                error={Boolean(touched.state && errors.state)}
+                helperText={touched.state && errors.state}
+              />
+            </>
+          ) : null}
 
           <LoadingButton
-            style={{width:'150px',alignSelf:'center'}}
+            style={{ width: "150px", alignSelf: "center" }}
             size="large"
             type="submit"
             variant="contained"
             loading={isSubmitting && load}
+            disabled={!isPhoneVerified}
           >
             Register
           </LoadingButton>
